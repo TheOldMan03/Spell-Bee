42d6e071dc7afb3c6a42e6873583ac126f13768d
Joel Antony <joel.geoanto@gmail.com>
2024-07-14 13:23:13 +0530

    Completed UI of page

--- src/app/page.js
+++ src/app/page.js
@@ -17,11 +17,26 @@ export default function Home() {
 
         <div className="mt-6 bg-[#EEF2F8] min-h-[50px] w-[40%]"></div> 
 
-        {/* <div className="">This is going to be my text area</div>
+        <div className="mt-6">
+          <textarea rows={1} cols={15} maxlength={15} className="bg-black resize-none border-none text-center text-4xl caret-yellow-400 outline-none overflow-hidden"></textarea>
+        </div>
 
-        <div className="">Hexagon Lists</div>
+        <div className="max-w-[60%] max-h-[40%] flex justify-evenly mt-6 min-w-"> {/* This needs to be changed in the end...for now this is temp*/}
+          <div className="w-24 h-24 rounded-full bg-white text-black mx-2 text-2xl flex justify-center items-center">H</div>
 
-        <div className="">The various buttons</div> */}
+          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">E</div>
+          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">X</div>
+          <div className="w-24 h-24 rounded-full bg-yellow-400 text-black text-center mx-2 text-2xl flex justify-center items-center">A</div>
+          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">G</div>
+          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">O</div>
+          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">N</div>
+        </div>
+
+        <div className="flex mt-12">
+          <div className="w-24 h-12 bg-white text-black text-center rounded-full flex justify-center items-center mx-4">Delete</div>
+          <div className="w-12 h-12 rounded-full bg-white text-black flex justify-center items-center">R</div>
+          <div className="w-24 h-12 bg-white text-black text-center rounded-full flex justify-center items-center mx-4">Enter</div>
+        </div>
 
     </div>
   );
