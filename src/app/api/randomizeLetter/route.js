7a6d97c9d2ceaac1af1623f8f45cb71d76fd4b38
Joel Antony <joel.geoanto@gmail.com>
2024-07-19 20:12:17 +0530

    Optimized and corrected the API

--- src/app/api/randomizeLetter/route.js
+++ src/app/api/randomizeLetter/route.js
@@ -4,7 +4,7 @@ import fs from 'fs'
 import path from "path"
 
 export function GET(){
-    let randomWord=words7[Math.floor(Math.random() * 3087)];
+    let randomWord="rankling"  //words7[Math.floor(Math.random() * 3087)];
     // let randomLetter=randomWord[Math.floor(Math.random()*7)]
 
     let charList=randomWord.split('')
@@ -13,60 +13,93 @@ export function GET(){
     return NextResponse.json({"allWords":finalWords,"letters":charList});
 }
 
-function intersection(listwords){
-    let set1 = new Set(listwords[0]);
-    let set2 = new Set(listwords[1]);
-    let set3 = new Set(listwords[2]);
-    let set4 = new Set(listwords[3]);
-    let set5 = new Set(listwords[4]);
-    let set6 = new Set(listwords[5]);
-    let set7 = new Set(listwords[6]);
 
+// function filterWords(Words,charList){
+//     let finalWords=[]
+//     const letter_set=new Set(charList);
 
-    let finalSet = new Set();
-    set1.forEach(item => {
-        if (set2.has(item) && set3.has(item) && set4.has(item) && set5.has(item) && set6.has(item) && set7.has(item)) {
-            finalSet.add(item);
-        }
-    });
+//     for(let i=0;i<Words.length;i++){
+//         let word=Words[i];
+//         for(let j=0;j<word.length;j++){
+//             if(!letter_set.has(word[j])){
+//                 break;
+//             }
+//             if(j==word.length-1){
+//                 finalWords.push(word);
+//             }
+//         }    
+//     }
 
-    let Words=Array.from(finalSet);
-    return Words;
-}
-
-function filterWords(Words,charList){
-    let finalWords=[]
-    const letter_set=new Set(charList);
-
-    for(let i=0;i<Words.length;i++){
-        let word=Words[i];
-        for(let j=0;j<word.length;j++){
-            if(!letter_set.has(word[j])){
-                break;
-            }
-            if(j==word.length-1){
-                finalWords.push(word);
-            }
-        }    
-    }
-
-    return finalWords;
-}
+//     return finalWords;
+// }
 
 function getList(charList){
     let L=[]
     const str=path.join(process.cwd(),'Words')
     console.log(process.cwd())
 
-    for(let i=0;i<charList.length;i++){
-        let filepath=path.join(str,charList[i]+'.txt')
-        const data=fs.readFileSync(filepath,'utf8');
-        const w=data.split('\n').map((word)=>word.trim());
+    // for(let i=0;i<charList.length;i++){
+    //     let filepath=path.join(str,charList[i]+'.txt')
+    //     const data=fs.readFileSync(filepath,'utf8');
+    //     const w=data.split('\n').map((word)=>word.trim());
     
-        L.push(w);
+    //     L.push(w);
+    // }
+
+    // let set_words=new Set(charList)
+    // let finalWords=[]
+    // let flag;
+
+    // for(let i=0;i<7;i++){
+    //     for(let j=0;j<L[i].length;j++){
+    //         flag=1;
+    //         for(let k=0;k<L[i][j].length;k++){
+    //             if(!set_words.has(L[i][j][k])){
+    //                 flag=0;
+    //                 break;
+    //             }
+    //         }
+    //         if(flag==1){
+    //             finalWords.push(L[i][j]);
+    //     }   }
+    // }
+
+    const wordDict = {};
+    
+    // Read all files and store the words in a dictionary
+    for (let char of charList) {
+        let filepath = path.join(str, char + '.txt');
+        if (!wordDict[char]) {
+            const data = fs.readFileSync(filepath, 'utf8');
+            wordDict[char] = data.split('\n').map(word => word.trim());
+        }
+    }
+    
+    // Create a set of all words from the files
+    let allWords = new Set();
+    for (let char of charList) {
+        for (let word of wordDict[char]) {
+            allWords.add(word);
+        }
+    }
+    
+    // Check if each word can be made using the characters in charList
+    let finalWords = [];
+    let setWords = new Set(charList);
+    
+    for (let word of allWords) {
+        let flag = true;
+        for (let char of word) {
+            if (!setWords.has(char)) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) {
+            finalWords.push(word);
+        }
     }
 
-    const intermdiateWords=intersection(L);
-    return filterWords(intermdiateWords,charList);
-    // return intermdiateWords;
+
+    return finalWords;
 }
