7f82bbf7607075d5e81fcf8cc94f0b3bf3b338ea
Joel Antony <joel.geoanto@gmail.com>
2024-07-21 11:40:39 +0530

    Letters not being displayed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,33 @@ import axios from "axios";
 
 export default async function Home() {
 
-  const resp=await axios.get("http://localhost:3000/api/randomizeLetter");
-  const {letters}=resp.data;
+  const ISSERVER = typeof window === "undefined";
 
+  const getCurrTime=()=>{
+    const time_now=new Date();
+
+    let h=time_now.getHours();
+    let m=time_now.getMinutes();
+    let s=time_now.getSeconds();
+
+    const am_pm=h>=12?"PM":"AM";
+    h=h>12?h-12:h;
+    h=h?h:12;
+
+    const mstr=m<10?"0"+m:m;
+    const sstr=s<10?"0"+s:s;
+
+    const time_str=`${h}:${mstr}:${sstr} ${am_pm}`;
+    return time_str;
+  }
+
+  if(!ISSERVER && (localStorage.getItem("letter_arr")===null || getCurrTime()==="12:00:00 AM")){
+    const resp=await axios.get("http://localhost:3000/api/randomizeLetter");
+    const {letters}=resp.data;
+    localStorage.setItem("letter_arr",JSON.stringify(letters)); 
+  }
+
+  const letters=ISSERVER?null:JSON.parse(localStorage.getItem("letter_arr"));
   return (
     // main div
     <div className="flex flex-col items-center">
@@ -28,13 +52,13 @@ export default async function Home() {
         </div>
 
         <div className="max-w-[60%] max-h-[40%] flex justify-evenly mt-6 min-w-"> {/* This needs to be changed in the end...for now this is temp*/}
-          <div className="w-24 h-24 rounded-full bg-white text-black mx-2 text-2xl flex justify-center items-center">{letters[0]}</div>
-          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">{letters[1]}</div>
-          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">{letters[2]}</div>
-          <div className="w-24 h-24 rounded-full bg-yellow-400 text-black text-center mx-2 text-2xl flex justify-center items-center">{letters[3]}</div>
-          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">{letters[4]}</div>
-          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">{letters[5]}</div>
-          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">{letters[6]}</div>
+          <div className="w-24 h-24 rounded-full bg-white text-black mx-2 text-2xl flex justify-center items-center">{ISSERVER?null:letters[0]}</div>
+          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">{ISSERVER?null:letters[1]}</div>
+          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">{ISSERVER?null:letters[2]}</div>
+          <div className="w-24 h-24 rounded-full bg-yellow-400 text-black text-center mx-2 text-2xl flex justify-center items-center">{ISSERVER?null:letters[3]}</div>
+          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">{ISSERVER?null:letters[4]}</div>
+          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">{ISSERVER?null:letters[5]}</div>
+          <div className="w-24 h-24 rounded-full bg-white text-black text-center mx-2 text-2xl flex justify-center items-center">{ISSERVER?null:letters[6]}</div>
         </div>
 
         <div className="flex mt-12">
